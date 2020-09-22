dd69afaec682ca17117daaa1af1fe1427f72f21e
Lei Sosa <leisosa93@gmail.com>
2020-09-21 22:42:10 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.video-container video');
+
+    // sonidos
+    const sounds = document.querySelectorAll('.sound-picker button');
+    // time display
+    const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
+    // circunferencia
+    const outlineLength = outline.getTotalLength();
+    // duracion
+    let fakeDuration = 600;
+
+    // obtengo la circunferencia
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // seleccionar otro sonido
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function () {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
+    });
+    // play
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // elegir la duracion
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function () {
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+        });
+    });
+
+    // funcion para play y pause y cambia los iconos
+    const checkPlaying = song => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    };
+
+    // animacion del borde play
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        // animacion del borde
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        // animacion del texto
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if (currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        }
+    };
+};
+
+app();
